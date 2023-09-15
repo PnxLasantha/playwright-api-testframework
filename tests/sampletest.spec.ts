40b9717880cf9c3f2e03d58a3c55c978b6060a04
lcaldera <lasantha.caldera@dualinventive.com>
2023-09-15 15:23:05 +0530

    removed pnpm

--- tests/sampletest.spec.ts
+++ tests/sampletest.spec.ts
@@ -1,14 +1,14 @@
-import test from "@playwright/test";
-import GetRequestBuilder from "../helper/requestBuilders/requestBuilder";
+import test from '@playwright/test'
+import GetRequestBuilder from '../helper/requestBuilders/requestBuilder'
 
-test("sampleGet", async () => {
-  const query = {
-    per_page: 1,
-  };
-  const url = "https://api.github.com/repos/Microsoft/playwright/pulls";
-  const get = new GetRequestBuilder();
-  const res = await get.getRequest(url, {
-    queryParameters: query,
-  });
-  console.log(res.status());
-});
+test('sampleGet', async () => {
+    const query = {
+        per_page: 1,
+    }
+    const url = 'https://api.github.com/repos/Microsoft/playwright/pulls'
+    const get = new GetRequestBuilder()
+    const res = await get.getRequest(url, {
+        queryParameters: query,
+    })
+    console.log(res.status())
+})
